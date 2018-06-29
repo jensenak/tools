c0d42be98ec1831ad092e759431e0dcbd16e6115
Adam Jensen <jensen.ak@gmail.com>
2018-06-29 16:38:59 -0600

    Simplify tab coloring, prompt generation

--- prompt.sh
+++ prompt.sh
@@ -40,23 +40,16 @@ bright="\033[0;32;100m"
 light="\033[0;33;100m"
 mid="\033[0;33;100m"
 dark="\033[0;30;100m"
-nobg="\033[1;35m"
+n1="\033[1;35m"
+n2="\033[1;36m"
+n3="\033[1;33m"
 
 #The actual prompt that will be used
-if [[ -n $VIRTUAL_ENV ]] || [[ -n $(__git_ps1) ]]
-then
-	p1=' '
-    p2=' '
-	if [[ -n $VIRTUAL_ENV ]]
-	then 
-		p1=$(basename "$VIRTUAL_ENV")
-	fi
-	if [[ -n $(__git_ps1) ]]
-	then
-		p2=$(__git_ps1)
-	fi
-	PS1="\n$nobg$p1$p2$reset\n$dark$filler $mid$rooty$bright$mwd$dark$fill $light$day$reset\n>> "
-else
-	PS1="\n$dark$filler $mid$rooty$bright$mwd$dark$fill $light$day$reset\n>> "
+p1=' '
+if [[ -n $VIRTUAL_ENV ]]
+then 
+    p1=$(basename "$VIRTUAL_ENV")
 fi
+p2=$(__git_ps1)
+PS1="\n $n3$p1$n1$p2 $n2$AWS_PROFILE $AWS_DEFAULT_REGION$reset\n$dark$filler $mid$rooty$bright$mwd$dark$fill $light$day$reset\n>> "
 }
